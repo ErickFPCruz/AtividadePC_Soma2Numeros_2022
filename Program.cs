94d58ae826bcd3191f56fc2087b836b132465309
ErickFPCruz <erick.cruz97@hotmail.com>
2022-03-25 21:41:24 -0300

    Calculadora de Somas

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+int numero1, numero2, somaDosNumeros;
+
+Console.Write("Escreva um número qualquer: ");
+numero1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Escreva o número que será somado ao anterior: ");
+numero2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+somaDosNumeros = numero1 + numero2;
+
+Console.Write($"Soma = {somaDosNumeros}");
+Console.WriteLine();
